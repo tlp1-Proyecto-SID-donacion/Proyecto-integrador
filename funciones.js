f6b995af155ac2be42d2bb830f618d1215df2f21
David Liva <davidliva01@gmail.com>
2023-06-09 01:21:06 -0300

    el se a cambiado el diseño de la página, el aside se a eliminado y se agregó un section, un archivo.js, pruebas de animaciones en la hoja de estilos, y una carpeta que contiene imagenes de prueba

---
+++ funciones.js
@@ -0,0 +1,17 @@
+// Selecciona el botón
+const button = document.getElementById('myButton');
+
+// Selecciona el elemento que deseas animar
+const contenido = document.querySelector('.contenido-2');
+
+// Agrega un evento de clic al botón
+button.addEventListener('click', function() {
+  // Agrega la clase "animacion" al elemento
+  contenido.classList.add('.contenido-2');
+});
+
+
+
+
+
+
